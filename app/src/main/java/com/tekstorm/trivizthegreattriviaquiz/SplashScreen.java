5c6fb903b106ab6c18e27cc8d3a3dbde83a8d990
Anusha Bhattacharya <47797609+anusha267@users.noreply.github.com>
2020-08-30 22:04:32 +0530

    30/8

--- app/src/main/java/com/tekstorm/trivizthegreattriviaquiz/SplashScreen.java
+++ app/src/main/java/com/tekstorm/trivizthegreattriviaquiz/SplashScreen.java
@@ -2,7 +2,13 @@ package com.tekstorm.trivizthegreattriviaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.triviz_animation);
+        textView.setAnimation(anim);
+        textView2.setAnimation(anim);
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            }
+        }).start();
     }
 }
