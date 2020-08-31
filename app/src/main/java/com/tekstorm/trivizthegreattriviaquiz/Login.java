edb825b541fc4f6a96a487f85c4b96fb83d75ace
Anusha Bhattacharya <47797609+anusha267@users.noreply.github.com>
2020-08-31 14:26:51 +0530

    30/8

--- app/src/main/java/com/tekstorm/trivizthegreattriviaquiz/Login.java
+++ app/src/main/java/com/tekstorm/trivizthegreattriviaquiz/Login.java
@@ -1,29 +1,41 @@
 package com.tekstorm.trivizthegreattriviaquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     EditText email, password;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onStart() {
         super.onStart();
-
-
-
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null)
+        {
+            startActivity(new Intent(Login.this,MainActivity.class));
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth = FirebaseAuth.getInstance();
 
 
 
@@ -32,7 +44,26 @@ public class Login extends AppCompatActivity {
     }
 
     public void signIn(View view) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("signin", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
 
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
 
 
 
